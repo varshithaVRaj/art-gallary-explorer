16c7f5246394d8738b5d1a113e5f6c7e763716f9
varshitha v <varshitha.vraj@winds.co.in>
2026-07-12 19:02:46 +0530

    add xcode project

---
+++ ArtGalaryExplorer_SwiftUI/ArtGalaryExplorer_SwiftUI/ArtGalaryExplorer_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArtGalaryExplorer_SwiftUIApp.swift
+//  ArtGalaryExplorer_SwiftUI
+//
+//  Created by Varshitha VRaj on 12/07/26.
+//
+
+import SwiftUI
+
+@main
+struct ArtGalaryExplorer_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ArtGalaryExplorer_SwiftUI/ArtGalaryExplorer_SwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ArtGalaryExplorer_SwiftUI
+//
+//  Created by Varshitha VRaj on 12/07/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
