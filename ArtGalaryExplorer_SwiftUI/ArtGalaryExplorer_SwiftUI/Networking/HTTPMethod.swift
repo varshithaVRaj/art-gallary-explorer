8c52f8ff513ab9cf0ff4412e7d40885824a29c39
varshitha v <varshitha.vraj@winds.co.in>
2026-07-12 19:21:34 +0530

    add files

---
+++ ArtGalaryExplorer_SwiftUI/ArtGalaryExplorer_SwiftUI/Networking/HTTPMethod.swift
@@ -0,0 +1,16 @@
+//
+//  HTTPMethod.swift
+//  ArtGalaryExplorer_SwiftUI
+//
+//  Created by Varshitha VRaj on 12/07/26.
+//
+
+import SwiftUI
+
+enum HTTPMethod: String {
+    case get = "GET"
+    case post = "POST"
+    case put = "PUT"
+    case delete = "DELETE"
+    case patch = "PATCH"
+}
